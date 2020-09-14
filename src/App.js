cffd5ef5eb6fda81e1f25f43b14fe4126664ddaf
Ankit Negi <ankit.negi@calsoftinc.com>
2020-09-14 10:42:10 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/main.scss';
+import EpicMain from './components/EpicMain';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import Faq from './components/Faq';
+import News from './components/News';
+import Unreal from './components/Unreal';
+import Help from './components/Help';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path='/' render={() => <Redirect from="/" to="/store" ></Redirect>} />
+          <Route path='/store' render={(props) => <EpicMain {...props} />} />
+          <Route path='/faq' render={(props) => <Faq {...props} />} />
+          <Route path='/news' render={(props) => <News {...props} />} />
+          <Route path='/help' render={(props) => <Help {...props} />} />
+          <Route path='/unreal' render={(props) => <Unreal {...props} />} />
+        </Switch>
+      </Router>
     </div>
   );
 }
